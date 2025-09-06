b59c590981856cf36ac31201ae057aacd895a012
richardSHkim <kshtrue95@gmail.com>
2025-09-06 20:09:56 +0900

    update environ variable name

--- src/notice_helper/app/slack.py
+++ src/notice_helper/app/slack.py
@@ -3,5 +3,5 @@ import requests
 
 
 def call_slack(text: str):
-    url = os.environ.get("SLACK_ALARM_URL", None)
+    url = os.environ.get("SLACK_WEBHOOK_URL", None)
     requests.post(url, json={"text": text})
