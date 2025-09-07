4b524b12d8182bf05fcfc110fba2fb04ba661580
richardSHkim <kshtrue95@gmail.com>
2025-09-07 17:12:16 +0900

    add AT

--- src/alert_helper/alert.py
+++ src/alert_helper/alert.py
@@ -1,7 +1,7 @@
 import traceback
 from typing import Callable, Any
 
-from .config import APP, ON
+from .config import AT, ON, APP
 from .app import Slack
 
 
@@ -9,11 +9,13 @@ class alert:
     def __init__(
         self,
         text: str,
+        at: AT = AT.BOTH,
         on: ON = ON.ALWAYS,
         app: APP = APP.SLACK,
         verbose: bool = False,
     ):
         self.text = text
+        self.at = at
         self.on = on
         self.app = app
         self.verbose = verbose
@@ -27,19 +29,25 @@ class alert:
 
     # Context manager
     def __enter__(self):
+        if self.at in (AT.BOTH, AT.START):
+            text_start = "START: " + self.text
+            self.caller(text_start)
         return self
 
     def __exit__(self, exc_type, exc, tb):
-        # replace text with traceback if exception is occurred
-        if exc:
-            self.text = "".join(traceback.format_exception(exc_type, exc, tb))
+        if self.at in (AT.BOTH, AT.END):
+            # replace text with traceback if exception is occurred
+            if exc:
+                text_end = "".join(traceback.format_exception(exc_type, exc, tb))
+            else:
+                text_end = "END: " + self.text
 
-        if (
-            self.on == ON.ALWAYS
-            or (self.on == ON.SUCCESS and exc is None)
-            or (self.on == ON.ERROR and exc)
-        ):
-            self.caller(self.text)
+            if (
+                self.on == ON.ALWAYS
+                or (self.on == ON.SUCCESS and exc is None)
+                or (self.on == ON.ERROR and exc)
+            ):
+                self.caller(text_end)
         return False
 
     # Decorator
